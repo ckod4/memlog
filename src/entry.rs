bcbc6a62c32103aef8a75dcbd13362560e9020bc
Christian Koda <52430910+KODA-Christian@users.noreply.github.com>
2026-04-22 22:14:02 +0000

    feat: add a constructor to Entry struct

--- src/entry.rs
+++ src/entry.rs
@@ -1,4 +1,16 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
 struct Entry {
     timestamp: u64,
     payload: String,
+}
+
+impl Entry {
+    pub fn new(payload: String) -> Self {
+        let timestamp = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Operating system clock needs settings, it is running at a time before UNIX_EPOCH !")
+            .as_millis() as u64;
+        Entry { timestamp, payload }
+    }
 }
