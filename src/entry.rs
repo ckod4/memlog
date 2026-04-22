828a8122e345ef479ad8375f7ce0ff3c8ee69164
Christian Koda <52430910+KODA-Christian@users.noreply.github.com>
2026-04-22 23:10:56 +0000

    feat: preserve timestamp between storing and restoring of an Entry

--- src/entry.rs
+++ src/entry.rs
@@ -16,21 +16,26 @@ impl Entry {
 
     pub fn to_bytes(&self) -> Vec<u8> {
         let mut bytes = Vec::new();
-        bytes.extend_from_slice(self.payload.as_bytes());
+        // Prefix with a fixed-size 8-byte header (Timestamp)
+        // This allows the decoder to easily slice the first 8 bytes before reading the payload.
+        bytes.extend_from_slice(&self.timestamp.to_le_bytes());
+        bytes.extend_from_slice(&self.payload.as_bytes());
         bytes
     }
 
-    pub fn from_bytes(data: &Vec<u8>) -> Self {
-        let timestamp = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .expect("Operating system clock needs settings, it is running at a time before UNIX_EPOCH !")
-            .as_millis() as u64;
-        let payload: String = String::from_utf8(data.to_vec())
-        .expect("Found invalid UTF-8 bytes");
-        Entry {
-            timestamp,
-            payload
+    pub fn from_bytes(data: &Vec<u8>) -> Result<Self, String> {
+        if data.len() < 8 {
+            // 8 (timestamp)
+            return Err(String::from("Invalid data length"));
         }
+        let timestamp_bytes_vec: [u8; 8] = [
+            data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
+        ];
+        let timestamp = u64::from_le_bytes(timestamp_bytes_vec);
+        let payload: String =
+            String::from_utf8((&data[8..]).to_vec()).expect("Found invalid UTF-8 bytes");
+        Ok(
+            Entry { timestamp, payload }
+        )
     }
 }
-
