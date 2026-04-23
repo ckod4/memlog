7c90caa46bf0b55e36b60f2c0745ee142b77643b
Christian Koda <52430910+KODA-Christian@users.noreply.github.com>
2026-04-23 00:16:37 +0000

    feat: switch Entry payload type to bytes (Vec<u8>) in order to handle different payload types

--- src/entry.rs
+++ src/entry.rs
@@ -1,12 +1,12 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
-struct Entry {
+pub struct Entry {
     timestamp: u64,
-    payload: String,
+    payload: Vec<u8>,
 }
 
 impl Entry {
-    pub fn new(payload: String) -> Self {
+    pub fn new(payload: Vec<u8>) -> Self {
         let timestamp = SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .expect("Operating system clock needs settings, it is running at a time before UNIX_EPOCH !")
@@ -19,11 +19,11 @@ impl Entry {
         // Prefix with a fixed-size 8-byte header (Timestamp)
         // This allows the decoder to easily slice the first 8 bytes before reading the payload.
         bytes.extend_from_slice(&self.timestamp.to_le_bytes());
-        bytes.extend_from_slice(&self.payload.as_bytes());
+        bytes.extend_from_slice(&self.payload);
         bytes
     }
 
-    pub fn from_bytes(data: &Vec<u8>) -> Result<Self, String> {
+    pub fn from_bytes(data: &[u8]) -> Result<Self, String> {
         if data.len() < 8 {
             // 8 (timestamp)
             return Err(String::from("Invalid data length"));
@@ -32,10 +32,7 @@ impl Entry {
             data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
         ];
         let timestamp = u64::from_le_bytes(timestamp_bytes_vec);
-        let payload: String =
-            String::from_utf8((&data[8..]).to_vec()).expect("Found invalid UTF-8 bytes");
-        Ok(
-            Entry { timestamp, payload }
-        )
+        let payload: Vec<u8> = data[8..].to_vec();
+        Ok(Entry { timestamp, payload })
     }
 }
